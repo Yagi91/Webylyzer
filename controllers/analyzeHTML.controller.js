0cc65b2d04a576e9d5211b7ceae3f245588bdd2a
BryanTimah <bryan.neoworlder@proton.me>
2024-08-10 22:34:29 +0100

    Complete the structure of basic htmlanalyzer

--- controllers/analyzeHTML.controller.js
+++ controllers/analyzeHTML.controller.js
@@ -2,6 +2,9 @@ const { analyzeHTML } = require("../helpers/analyze");
 
 const readAnalyzedHTML = async (req, res) => {
   const { url } = req.body;
+  if (!url) {
+    return res.status(400).json({ error: "URL is required" });
+  }
   try {
     const result = await analyzeHTML(url);
     return res.status(200).json(result);
